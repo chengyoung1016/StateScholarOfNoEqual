ced4dd4d26678b1dee6f6b91a8992d059dcee68b
chengyoung1016 <93090261@qq.com>
2020-04-01 19:22:28 +0800

    提交配置文件

---
+++ assets/Script/MainController.ts
@@ -0,0 +1,19 @@
+const {ccclass, property} = cc._decorator;
+
+@ccclass
+export default class NewClass extends cc.Component {
+    /**
+     * 组件首字母大写+组件类型  
+     * 需要显示在编辑器上的变量首字母大写
+     * 不需显示在编辑器上的组件变量首字母小写
+     * 不需显示在编辑器上的普通变量首字母前加_小写
+     */
+
+    onLoad () {}
+
+    start () {
+
+    }
+
+    // update (dt) {}
+}
